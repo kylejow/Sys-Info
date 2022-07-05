390e9eb18dfee12d335dc45d97d9facfb806a60f
kylejow <77653395+kylejow@users.noreply.github.com>
2022-07-05 15:16:17 -0700

    Added cpu usage

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@ json              https://github.com/nlohmann/json
 hardware info     https://docs.microsoft.com/en-us/windows/win32/sysinfo/getting-hardware-information?redirectedfrom=MSDN
 ram usage         https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/ns-sysinfoapi-memorystatusex
 ram usage         https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-globalmemorystatusex
+Cpu usage (Jeremy Friesner) https://stackoverflow.com/questions/23143693/retrieving-cpu-load-percent-total-in-windows-with-c
 */
 
 #include "Hardware_Classes.h"
+
+static float CalculateCPULoad(unsigned long long idleTicks, unsigned long long totalTicks);
+static unsigned long long FileTimeToInt64(const FILETIME & ft);
+float GetCPULoad();
 
 int main(){
    //system information
@@ -21,6 +26,8 @@ int main(){
       system("cls");
       cout << "1. Print System Info\n"
            << "2. Monitor RAM usage\n"
+           << "3. Monitor Cpu usage\n"
+           << "4. Monitor all usage\n"
            << "\n\nq to exit" << endl;
       cin >> input;
       if(input == "1"){
@@ -37,14 +44,55 @@ int main(){
          while(1){
             system ("cls");
             GlobalMemoryStatusEx(&memStat);
-            cout << memStat.ullAvailPhys/1048576 << "/" << memStat.ullTotalPhys/1048576 << " MB (" << memStat.dwMemoryLoad << "%)"
+            cout << "RAM: " << memStat.ullAvailPhys/1048576 << "/" << memStat.ullTotalPhys/1048576 << " MB (" << memStat.dwMemoryLoad << "%)"
                  << "\n\n\nCtrl + C to exit" << endl;
             Sleep(50);
          }
-      }else if(input == "q"){
+      }else if(input == "3"){
+         while(1){
+            system ("cls");
+            cout << "Cpu: " << GetCPULoad()*100 << "%"
+                 << "\n\n\nCtrl + C to exit" << endl;
+            Sleep(50);
+         }
+      }else if(input == "4"){
+         while(1){
+            system ("cls");
+            GlobalMemoryStatusEx(&memStat);
+            cout << "RAM: " << memStat.ullAvailPhys/1048576 << "/" << memStat.ullTotalPhys/1048576 << " MB (" << memStat.dwMemoryLoad << "%)\n"
+                 << "Cpu: " << GetCPULoad()*100 << "%"
+                 << "\n\n\nCtrl + C to exit" << endl;
+            Sleep(50);
+         }
+      }else if(input == "q")
          break;
-      }
    }
    system ("cls");
    return 0;
+}
+
+static float CalculateCPULoad(unsigned long long idleTicks, unsigned long long totalTicks){
+   static unsigned long long _previousTotalTicks = 0;
+   static unsigned long long _previousIdleTicks = 0;
+
+   unsigned long long totalTicksSinceLastTime = totalTicks-_previousTotalTicks;
+   unsigned long long idleTicksSinceLastTime  = idleTicks-_previousIdleTicks;
+
+   float ret = 1.0f-((totalTicksSinceLastTime > 0) ? ((float)idleTicksSinceLastTime)/totalTicksSinceLastTime : 0);
+
+   _previousTotalTicks = totalTicks;
+   _previousIdleTicks = idleTicks;
+   return ret;
+}
+
+static unsigned long long FileTimeToInt64(const FILETIME & ft){
+   return (((unsigned long long)(ft.dwHighDateTime))<<32)|((unsigned long long)ft.dwLowDateTime);
+}
+
+// Returns 1.0f for "CPU fully pinned", 0.0f for "CPU idle", or somewhere in between
+// You'll need to call this at regular intervals, since it measures the load between
+// the previous call and the current one.  Returns -1.0 on error.
+float GetCPULoad(){
+   FILETIME idleTime, kernelTime, userTime;
+   return GetSystemTimes(&idleTime, &kernelTime, &userTime) ? CalculateCPULoad(FileTimeToInt64(idleTime), FileTimeToInt64(kernelTime)+FileTimeToInt64(userTime)) : -1.0f;
 }
